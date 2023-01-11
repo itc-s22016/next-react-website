82ae546cf328ccd174d0f67fde2ed08cbe4f6b2b
itc-s22016 <s22016@std.it-college.ac.jp>
2023-01-11 09:58:23 +0000

    aaaaa

---
+++ components/contact.js
@@ -0,0 +1,13 @@
+import styles from 'styles/contact.module.css'
+
+export default function Contact() {
+	return (
+		<div className={styles.stack}>
+		<h3 className={styles.heading}>Contact</h3>
+		<address>cub@web.mail.address</address>
+
+		</div>
+	)
+}
+
+
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,14 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Blog() {
-	return (<Hero
+	return (
+	   <Container>
+		<Hero
 		title="Blog"
 		subtitle="Recent Posts"
 		/>
+	  </Container>
 	)
 }
 
